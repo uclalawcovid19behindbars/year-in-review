60119ab9e7b22dd94a8691c6431ebe30c42a96bd
hjohns12 <hopecaneel@gmail.com>
2021-05-25 18:23:32 -0400

    chore: fix number formatting

--- js/CaseCount.js
+++ js/CaseCount.js
@@ -5,28 +5,28 @@ class CaseCount {
     }
   
     draw(state, setGlobalState) {
-      console.log("now I am drawing my CaseCount");
 
       const myCount = this.container
         .append("p")
         .style("font-family", "var(--sans-serif)")
-        .style("font-variant-numeric", "tabular-nums");
+        .style("font-variant-numeric", "tabular-nums")
+        .property("_current", 395529)
 
-      const format = d3.format(",.2r") // where to put this? 
+      const formatNumber = d3.format(",.2r") 
 
       myCount.transition()
         .ease(d3.easeLinear)
         .duration(state.transition)
         .tween("text", function(d) {
           var that = this;
-          var i = d3.interpolate(0, 395529);  // Number(d.percentage.slice(0, -1))
+          var i = d3.interpolate(0, 395529);  
           return function(t) {
               d3.select((that))
-                .text(i(t)
-                .toFixed(0)
+                .text(formatNumber(i(t))
                 );
           };
         })
+
     }
   }
   
